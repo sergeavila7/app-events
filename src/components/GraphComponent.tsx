f69ba2f305bd78d206373d6cd0e2c5d7686fd0b3
sergeavila7 <serge_avila7@yahoo.com>
2023-11-29 16:09:17 -0600

    fix: ts in graphcomponent

--- src/components/GraphComponent.tsx
+++ src/components/GraphComponent.tsx
@@ -1,18 +1,19 @@
 import Plot from 'react-plotly.js';
 import { useEvents } from '../hooks/useEvents';
 import dayjs from 'dayjs';
+
 const GraphComponent = () => {
   const { events, isSmallScreen } = useEvents();
 
   const months = events.map((event) => dayjs(event.dateTime).format('MMMM'));
 
-  const monthCounts = months.reduce((acc, month) => {
+  const monthCounts: Record<string, number> = months.reduce((acc, month) => {
     acc[month] = (acc[month] || 0) + 1;
     return acc;
-  }, {});
+  }, {} as Record<string, number>); // Explicit type definition for acc
 
-  const values = Object.values(monthCounts);
-  const labels = Object.keys(monthCounts);
+  const values: number[] = Object.values(monthCounts);
+  const labels: string[] = Object.keys(monthCounts);
 
   return (
     <Plot
